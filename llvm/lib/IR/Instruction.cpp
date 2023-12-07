bd3c748d4333431dcc98bd6389d1fc154e4c5d23
OCHyams <orlando.hyams@sony.com>
2023-12-07 10:40:55 +0000

    change instruction.h - can't use filtering getDbgValues here because can't get the return type without including DebugProgramInstruction.h

--- llvm/lib/IR/Instruction.cpp
+++ llvm/lib/IR/Instruction.cpp
@@ -242,8 +242,7 @@ Instruction::cloneDebugInfoFrom(const Instruction *From,
   return DbgMarker->cloneDebugInfoFrom(From->DbgMarker, FromHere, InsertAtHead);
 }
 
-iterator_range<DPValue::self_iterator>
-Instruction::getDbgValueRange() const {
+iterator_range<DPEntity::self_iterator> Instruction::getDbgValueRange() const {
   BasicBlock *Parent = const_cast<BasicBlock *>(getParent());
   assert(Parent && "Instruction must be inserted to have DPValues");
   (void)Parent;
@@ -254,7 +253,8 @@ Instruction::getDbgValueRange() const {
   return DbgMarker->getDbgEntityRange();
 }
 
-std::optional<DPValue::self_iterator> Instruction::getDbgReinsertionPosition() {
+std::optional<DPEntity::self_iterator>
+Instruction::getDbgReinsertionPosition() {
   // Is there a marker on the next instruction?
   DPMarker *NextMarker = getParent()->getNextMarker(this);
   if (!NextMarker)
