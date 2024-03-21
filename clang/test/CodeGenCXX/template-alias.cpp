0ecb2856af4bc5529afa87cac1c3cffa933daf16
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2024-03-21 13:45:05 +0000

    add template value check

--- clang/test/CodeGenCXX/template-alias.cpp
+++ clang/test/CodeGenCXX/template-alias.cpp
@@ -1,21 +1,22 @@
 // RUN: %clang_cc1 -triple x86_64-unk-unk -o - -emit-llvm -debug-info-kind=standalone -debugger-tuning=sce %s | FileCheck %s --check-prefixes=SCE,ALL
 // RUN: %clang_cc1 -triple x86_64-unk-unk -o - -emit-llvm -debug-info-kind=standalone -debugger-tuning=gdb %s | FileCheck %s --check-prefixes=GDB,ALL
 
-template<typename T, typename U>
+//// Check that -gsce debugger tuning causes DW_TAG_template_alias emission
+//// for template aliases. Test type and value template parameters.
+template<typename Y, int Z>
 struct X {
-  T m1;
-  U m2;
+  Y m1 = Z;
 };
 
-template<typename V>
-using Y = X<V, int>;
+template<typename B, int C>
+using A = X<B, C>;
 
-Y<int> y = {1, 2};
+A<int, 5> a;
 
-// GDB: !DIDerivedType(tag: DW_TAG_typedef, name: "Y<int>", file: !5, line: 11, baseType: ![[baseType:[0-9]+]])
-// SCE: !DIDerivedType(tag: DW_TAG_template_alias, name: "Y", file: ![[#]], line: [[#]], baseType: ![[baseType:[0-9]+]], extraData: ![[extraData:[0-9]+]])
-// ALL: ![[baseType]] = distinct !DICompositeType(tag: DW_TAG_structure_type, name: "X<int, int>",
+// GDB: !DIDerivedType(tag: DW_TAG_typedef, name: "A<int, 5>", file: ![[#]], line: [[#]], baseType: ![[baseType:[0-9]+]])
+// SCE: !DIDerivedType(tag: DW_TAG_template_alias, name: "A", file: ![[#]], line: [[#]], baseType: ![[baseType:[0-9]+]], extraData: ![[extraData:[0-9]+]])
+// ALL: ![[baseType]] = distinct !DICompositeType(tag: DW_TAG_structure_type, name: "X<int, 5>",
 // ALL: ![[int:[0-9]+]] = !DIBasicType(name: "int", size: 32, encoding: DW_ATE_signed)
-// SCE: ![[extraData]] = !{![[V:[0-9]+]]}
-// SCE: ![[V]] = !DITemplateTypeParameter(name: "V", type: ![[int]])
-
+// SCE: ![[extraData]] = !{![[B:[0-9]+]], ![[C:[0-9]+]]}
+// SCE: ![[B]] = !DITemplateTypeParameter(name: "B", type: ![[int]])
+// SCE: ![[C]] = !DITemplateValueParameter(name: "C", type: ![[int]], value: i32 5)
