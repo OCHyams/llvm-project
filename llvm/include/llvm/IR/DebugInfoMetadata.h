efff5af6249007340df4b468995b04d0f973559c
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-03-24 17:23:59 +0000

    [KeyInstr][Inline] Don't propagate atoms to inlined nodebug instructions

--- llvm/include/llvm/IR/DebugInfoMetadata.h
+++ llvm/include/llvm/IR/DebugInfoMetadata.h
@@ -2134,6 +2134,13 @@ public:
     return 0;
   }
 
+  const DILocation *getOrCloneWithoutAtom() const {
+    if (!getAtomGroup() && !getAtomRank())
+      return this;
+    return get(getContext(), getLine(), getColumn(), getScope(), getInlinedAt(),
+               isImplicitCode());
+  }
+
   // Disallow replacing operands.
   void replaceOperandWith(unsigned I, Metadata *New) = delete;
 
