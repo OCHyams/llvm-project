f248da72546f78cbc5a0a8e8efa4af0436fdaaa5
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-03-31 17:23:27 +0100

    fixup, go with Clang level flag so we can add -mllvm flag easily, help-hidden

--- clang/test/KeyInstructions/flag.cpp
+++ clang/test/KeyInstructions/flag.cpp
@@ -1,7 +1,15 @@
-// RUN: %clang -### -target x86_64 -c -gdwarf-5 -Xclang -gkey-instructions %s 2>&1 | FileCheck %s --check-prefixes=KEY-INSTRUCTIONS
-// RUN: %clang -### -target x86_64 -c -gdwarf-5 -Xclang -gno-key-instructions %s 2>&1 | FileCheck %s --check-prefixes=NO-KEY-INSTRUCTIONS
-// KEY-INSTRUCTIONS: "-gkey-instructions"
-// NO-KEY-INSTRUCTIONS: "-gno-key-instructions"
-// NO-KEY-INSTRUCTIONS-NOT: "-gkey-instructions"
+// RUN: %clang -### -target x86_64 -c -gdwarf -gkey-instructions %s 2>&1 | FileCheck %s --check-prefixes=KEY-INSTRUCTIONS
+// RUN: %clang -### -target x86_64 -c -gdwarf -gno-key-instructions %s 2>&1 | FileCheck %s --check-prefixes=NO-KEY-INSTRUCTIONS
+//// Default: Off.
+// RUN: %clang -### -target x86_64 -c -gdwarf %s 2>&1 | FileCheck %s --check-prefixes=NO-KEY-INSTRUCTIONS
 
-// TODO: Add smoke test once some functionality has been added.
+//// Help hidden.
+// RUN %clang --help | FileCheck %s --check-prefix=HELP
+// HELP-NOT: key-instructions
+
+// KEY-INSTRUCTIONS: "-gkey-instructions"
+// KEY-INSTRUCTIONS: "-mllvm" "-dwarf-use-key-instructions"
+
+// NO-KEY-INSTRUCTIONS-NOT: key-instructions
+
+//// TODO: Add smoke test once some functionality has been added.
