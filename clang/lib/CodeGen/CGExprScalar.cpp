f019a7875779f48f6fa0a8629f851d802412a335
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-04-03 11:12:26 +0100

    [KeyInstr] Pre/Post Inc/Dec (assign test) - reword, just indicating to icinlude in assign patches

--- clang/lib/CodeGen/CGExprScalar.cpp
+++ clang/lib/CodeGen/CGExprScalar.cpp
@@ -2940,6 +2940,7 @@ public:
 llvm::Value *
 ScalarExprEmitter::EmitScalarPrePostIncDec(const UnaryOperator *E, LValue LV,
                                            bool isInc, bool isPre) {
+  ApplyAtomGroup Grp(CGF.getDebugInfo());
   OMPLastprivateConditionalUpdateRAII OMPRegion(CGF, E);
   QualType type = E->getSubExpr()->getType();
   llvm::PHINode *atomicPHI = nullptr;
