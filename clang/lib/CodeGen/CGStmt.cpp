a0d966e6a3562c15d8513718bc814d38407c57ab
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-04-03 19:01:20 +0100

    fixup while commit, reordering these is probably gonna explode after the renamings?

--- clang/lib/CodeGen/CGStmt.cpp
+++ clang/lib/CodeGen/CGStmt.cpp
@@ -1130,7 +1130,14 @@ void CodeGenFunction::EmitWhileStmt(const WhileStmt &S,
     if (!Weights && CGM.getCodeGenOpts().OptimizationLevel)
       BoolCondVal = emitCondLikelihoodViaExpectIntrinsic(
           BoolCondVal, Stmt::getLikelihood(S.getBody()));
-    Builder.CreateCondBr(BoolCondVal, LoopBody, ExitBlock, Weights);
+    auto *I = Builder.CreateCondBr(BoolCondVal, LoopBody, ExitBlock, Weights);
+    // Key Instructions: Emit the condition and branch as separate atoms to
+    // match existing loop stepping behaviour. FIXME: We could have the branch
+    // as the backup location for the condition, which would probably be a
+    // better experience. Explore this later.
+    if (auto *I = dyn_cast<llvm::Instruction>(BoolCondVal))
+      addInstToNewSourceAtom(I, nullptr);
+    addInstToNewSourceAtom(I, nullptr);
 
     if (ExitBlock != LoopExit.getBlock()) {
       EmitBlock(ExitBlock);
@@ -1355,14 +1362,7 @@ void CodeGenFunction::EmitForStmt(const ForStmt &S,
       BoolCondVal = emitCondLikelihoodViaExpectIntrinsic(
           BoolCondVal, Stmt::getLikelihood(S.getBody()));
 
-    auto *I = Builder.CreateCondBr(BoolCondVal, ForBody, ExitBlock, Weights);
-    // Key Instructions: Emit the condition and branch as separate atoms to
-    // match existing loop stepping behaviour. FIXME: We could have the branch
-    // as the backup location for the condition, which would probably be a
-    // better experience. Explore this later.
-    if (auto *I = dyn_cast<llvm::Instruction>(BoolCondVal))
-      addInstToNewSourceAtom(I, nullptr);
-    addInstToNewSourceAtom(I, nullptr);
+    Builder.CreateCondBr(BoolCondVal, ForBody, ExitBlock, Weights);
 
     if (ExitBlock != LoopExit.getBlock()) {
       EmitBlock(ExitBlock);
