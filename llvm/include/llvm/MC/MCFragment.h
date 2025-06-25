8b59950ebe55f4ad1585c8e29a6227f2e0523cf6
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2025-06-25 14:48:56 +0100

    Revert "add encoding kind situation"
    This reverts commit 0cc8a3680d048dc87bb7295ce915dbc4a4c0fdf7.

--- llvm/include/llvm/MC/MCFragment.h
+++ llvm/include/llvm/MC/MCFragment.h
@@ -471,17 +471,8 @@ public:
   const MCExpr *DiffStart;
   const MCExpr *DiffEnd;
 
-  enum EntryKindTy : uint8_t {
-    OffsetPair,
-    StartxLenght,
-    // Add more as needed.
-  } EntryKind;
-  uint8_t EntryKindEncoding;
-
   MCDwarfRangeListEntryFragment(MCContext &Context, const MCSymbol *Base,
-                                const MCSymbol *Begin, const MCSymbol *End,
-                                EntryKindTy EntryKind,
-                                uint8_t EntryKindEncoding);
+                                const MCSymbol *Begin, const MCSymbol *End);
 
   static bool classof(const MCFragment *F) {
     return F->getKind() == MCFragment::FT_DwarfLoclist;
--- llvm/lib/MC/MCObjectStreamer.cpp
+++ llvm/lib/MC/MCObjectStreamer.cpp
@@ -15,7 +15,6 @@
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCDwarf.h"
 #include "llvm/MC/MCExpr.h"
-#include "llvm/MC/MCFragment.h"
 #include "llvm/MC/MCObjectFileInfo.h"
 #include "llvm/MC/MCObjectWriter.h"
 #include "llvm/MC/MCSection.h"
@@ -132,8 +131,7 @@ MCObjectStreamer::emitDwarfLoclistElem(int8_t OffsetPair, const MCSymbol *Base,
 
   MCDwarfRangeListEntryFragment *ptr =
       getContext().allocFragment<MCDwarfRangeListEntryFragment>(
-          getContext(), Base, Begin, End,
-          MCDwarfRangeListEntryFragment::OffsetPair, OffsetPair);
+          getContext(), Base, Begin, End);
   insert(ptr);
   return ptr;
 }
@@ -142,16 +140,15 @@ MCDwarfRangeListOffsetPairFragment *
 MCObjectStreamer::emitDwarfRnglistElem(int8_t OffsetPair, const MCSymbol *Base,
                                        const MCSymbol *Begin,
                                        const MCSymbol *End) {
-  llvm_unreachable("removed this stuff");
-  // if (Base->getFragment() == Begin->getFragment() ||
-  //     Base->getFragment() == End->getFragment())
-  //   return MCStreamer::emitDwarfRnglistElem(OffsetPair, Base, Begin, End);
+  if (Base->getFragment() == Begin->getFragment() ||
+      Base->getFragment() == End->getFragment())
+    return MCStreamer::emitDwarfRnglistElem(OffsetPair, Base, Begin, End);
 
-  // MCDwarfRangeListOffsetPairFragment *ptr =
-  //     getContext().allocFragment<MCDwarfRangeListOffsetPairFragment>(
-  //         getContext(), Base, Begin, End);
-  // insert(ptr);
-  // return ptr;
+  MCDwarfRangeListOffsetPairFragment *ptr =
+      getContext().allocFragment<MCDwarfRangeListOffsetPairFragment>(
+          getContext(), Base, Begin, End);
+  insert(ptr);
+  return ptr;
 }
 
 void MCObjectStreamer::reset() {
