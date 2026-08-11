7753e79c514cfcdf1dd02f8003bbbe80d42639c7
Orlando Cazalet-Hyams <orlando.hyams@sony.com>
2026-08-11 17:03:42 +0100

    don't explode if there's an invalid string md node

--- llvm/lib/Bitcode/Reader/MetadataLoader.cpp
+++ llvm/lib/Bitcode/Reader/MetadataLoader.cpp
@@ -2066,7 +2066,9 @@ Error MetadataLoader::MetadataLoaderImpl::parseOneMetadata(
 
     DISubprogram *SP = nullptr;
 
-    bool MaybeODR = !IsDistinct && !(SPFlags & DISubprogram::SPFlagDefinition);
+    bool MaybeODR = !IsDistinct &&
+                    !(SPFlags & DISubprogram::SPFlagDefinition) &&
+                    getMDString(Record[3]);
     if (MaybeODR)
       SP = ODRUniquer.getODRSubprogramDecl(
           getDITypeRefOrNull(Record[1]), getMDString(Record[3])->getString(),
